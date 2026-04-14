bdd6e006b7af6e686f9cd53e777ba22f4645354d
LeTaras <purplesloth.riario1337@gmail.com>
2026-04-14 11:39:49 +0300

    move hello testing into component

--- todoapp.client/src/App.jsx
+++ todoapp.client/src/App.jsx
@@ -1,22 +1,13 @@
-import { useEffect, useState } from 'react'
+import { HelloTest } from "./components/HelloTest";
 
 function App() {
-  const [data, setData] = useState(null)
-
-  useEffect(() => {
-    fetch("/api/test")
-      .then(res => res.json())
-      .then(data => setData(data))
-  }, [])
-
-  let msg = data.message;
-  return (
-    <div>
-      <h1>React + .NET</h1>
-      <pre>{msg}</pre>
-      
-    </div>
-  )
+  return <HelloTest />;
 }
 
-export default App
+export default App;
+
+  // useEffect(() => {
+  //   fetch("/api/test")
+  //     .then((res) => res.json())
+  //     .then((data) => setData(data));
+  // }, []);
---
+++ todoapp.client/src/components/HelloTest.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+
+export function HelloTest() {
+  const [data, setData] = useState(null);
+
+  const getData = async (url) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+
+      const result = await response.json();
+      console.log(result);
+
+      return result;
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getData("/api/test");
+      setData(result);
+    };
+    fetchData();
+  }, []);
+
+  return (
+    <div>
+      <h1>React + .NET</h1>
+      <pre>{data.message}</pre>
+    </div>
+  );
+}
